94bddbc898ec6824762f3c81b81909ce8d558446
Peashooter-Real <pokpongp33@gmail.com>
2026-03-21 23:58:49 +0700

    Delete debug.js

--- debug.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('c:\\Users\\User\\Downloads\\vanguarad\\script.js', 'utf8');
-const lines = content.split('\n');
-const targetLine = lines[5716]; // 5717 in 1-indexed
-console.log(JSON.stringify(targetLine));
